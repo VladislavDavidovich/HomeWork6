a3b8cd6a7b543a1cd85e9542adbe37524be6edb0
Vladislav <Vlad.davidovich.2014@mail.ru>
2022-08-29 22:56:51 +0300

    первая задача

---
+++ Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Введите размер массива ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [number];
+FillArray(array);
+//PrintArray(array);
+
+void FillArray(int[] array)
+{
+    for(int i = 0; i < number; i++)
+    {
+    Console.Write($"Число {i+1} = ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+void PrintArray(int[] array)
+{
+int Numbers = 0;
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+    if(array[i] > 0 ) Numbers +=1;
+    {
+    return; 
+    }     
+    }
+}
+Console.WriteLine($"Количество чисел больше нуля = {Numbers}");
+}
+
